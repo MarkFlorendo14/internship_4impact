6a695f265138cfe36d6439db65313b969b30a932
Mark <markflorendo814@gmail.com>
2026-03-27 14:21:48 +0800

    added exercise 5

---
+++ src/exercise5/Main.java
@@ -0,0 +1,10 @@
+package exercise5;
+
+public class Main {
+    public static void main (String[] args){
+
+        bankAccount3 acc1 = new bankAccount3("Mark Angelo Florendo", 1000);
+
+        acc1.withdraw(2000, "I want to buy pc peripherals");
+    }
+}
