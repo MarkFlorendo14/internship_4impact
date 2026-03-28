604117a631fb080782f69770adfd6149eda7d18f
Mark <markflorendo814@gmail.com>
2026-03-29 00:02:53 +0800

    higher or lower feature

--- src/exercise6/NumberGuessingGame.java
+++ src/exercise6/NumberGuessingGame.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class NumberGuessingGame {
     Random rand = new Random();
-    int secretNumber = rand.nextInt(100);
+    int secretNumber = rand.nextInt(20);
     int wrongGuesses = 0;
     Scanner scanner = new Scanner(System.in);
 
@@ -13,15 +13,23 @@ public class NumberGuessingGame {
                 try {
                     System.out.println("Please enter a number: ");
                     int number = Integer.parseInt(scanner.nextLine());
+
+                    if (number > secretNumber) {
+                        System.out.println("Number too high, go lower!");
+                    } else if (number < secretNumber) {
+                        System.out.println("Number too low, go higher");
+                    } else {
+                        System.out.println("\n ********** Congratulations! **********");
+                        System.out.println("\n You guessed the number! The number is: " + secretNumber + "!");
+                        return;
+                    }
+
                     if (number != secretNumber) {
                         wrongGuesses++;
                         System.out.println("Wrong guess! You have " + (7 - wrongGuesses) + " left!");
                         if (wrongGuesses == 7) {
                                 throw new GameOverException(7);
                         }
-                    } else {
-                        System.out.println("You guessed the number! The number is: " + secretNumber + "!");
-                        return;
                     }
                 }   catch (NumberFormatException e){
                     System.out.println("Please enter a valid number!");
